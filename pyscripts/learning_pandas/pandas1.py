20acdc6f5bc8c385911108ee6168b6198494439c
kirmada67-dot <kirmada67@gmail.com>
2026-01-16 23:08:24 +0530

    todays work

--- pyscripts/learning_pandas/pandas1.py
+++ pyscripts/learning_pandas/pandas1.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 import pandas as pn
-
+import os
 #pan = pn.Series([1, 3, 4])
 #print(pan)
 
-data = {"age": [19, 21, 25], "salary": [200, 2000, 4000]}
+#data = {"sr.no.": [1, 2, 3], "age": [19, 21, 25], "salary": [200, 2000, 4000]}
 
-df = pn.DataFrame(data)
+#df = pn.DataFrame(data)
+
+df = pn.read_csv("test.csv")
+fullmarks = 100
 print(df)
+print(df["score"] >= 100)
+print(df.info())
+#print(df)
+
+#print(os.getcwd())
+
+#print(df)
