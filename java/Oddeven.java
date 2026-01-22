13707c0c3798ca99dce55f5496622fde3b3f0236
kirmada67-dot <kirmada67@gmail.com>
2026-01-22 14:19:23 +0530

    New code

--- java/Oddeven.java
+++ java/Oddeven.java
@@ -1,6 +1,17 @@
 import java.util.Scanner;
-import java.util.Math;
 
 class Oddeven {
-	public static void main(String[] args){
-		Scanner sc 
+        public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Enter number: ");
+                float n = sc.nextFloat();
+                if ( n % 2 == 0 ) {
+                        System.out.println(n + " is even number.");
+                }
+                else {
+                        System.out.println(n + " is odd number.");
+                }
+        }
+}
+
+ 
