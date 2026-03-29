8bee259a2dbc2a00ab7706d2954ef09e96555d74
kirmada67-dot <kirmada67@gmail.com>
2026-03-29 18:47:00 +0530

    cv score model

---
+++ pyscripts/learning_sklearn/cv_score/cv1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import pandas as pn
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import Lasso
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+df = pn.read_csv("../dummy_data/problem1.csv")
+model = Lasso()
+scaler = StandardScaler()
+
+x = df[["area", "rooms", "age"]]
+y = df["rent"]
+
+pipeline = Pipeline([("scaler", scaler), ("model", model)])
+
+scores = cross_val_score(pipeline, x, y, cv=7)
+print("Model: ", model)
+print(scores)
+print("Mean: ", scores.mean())
--- pyscripts/learning_sklearn/train-test-split/problem1.py
+++ pyscripts/learning_sklearn/train-test-split/problem1.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 import pandas as pn
-import numpy as np
 from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
 from sklearn.preprocessing import StandardScaler
 
 df = pn.read_csv("problem1.csv")
@@ -12,13 +11,15 @@ x = df[["area", "rooms", "age", "area_squared"]]
 y = df["rent"]
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2,  random_state=42)
-
+model = Lasso(alpha=1)
 scaler = StandardScaler()
+
+print(model)
+
+print("x_test: ", x_test)
 
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
-
-model = Ridge()
 
 model.fit(x_train, y_train)
 
@@ -26,7 +27,6 @@ print("coef_:", model.coef_)
 print("intercept_:", model.intercept_)
 
 pred = model.predict(x_test)
-
 print("Predictions: ", pred)
 
 print("MSE: ", mean_squared_error(y_test, pred))
