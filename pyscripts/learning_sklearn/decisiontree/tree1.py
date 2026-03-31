0b8d2c4dd81b1dc5a74cf79e97c4b1ceb2be5a09
kirmada67-dot <kirmada67@gmail.com>
2026-03-31 22:55:08 +0530

    testing tree more

--- pyscripts/learning_sklearn/decisiontree/tree1.py
+++ pyscripts/learning_sklearn/decisiontree/tree1.py
@@ -2,18 +2,22 @@
 import pandas as pn
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
-#from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score
 
-x = [[500], [800], [1200], [1500]]
-y = [10000, 15000, 25000, 30000]
+df = pn.read_csv("../dummy_data/problem1.csv")
 
-#x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=69)
+x = df[["area", "rooms", "age"]]
+y = df["rent"]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=69)
 
 model = DecisionTreeRegressor()
 
-model.fit(x, y)
+model.fit(x_train, y_train)
 
-pred = model.predict([[1100]])
+pred = model.predict(x_train)
 
-#print("x_test: ", x_test)
+print("Model: ", model)
 print("prediction: ", pred)
+print("MSE: ", mean_squared_error(y_train, pred))
+print("r2_score: ", r2_score(y_train, pred))
