d9dc5109efdc4d01adeefaba0ebfccbc28213b1c
kirmada67-dot <kirmada67@gmail.com>
2026-04-05 22:34:00 +0530

    tried xgb on large data set

--- pyscripts/learning_sklearn/xgbregressor/xgb1.py
+++ pyscripts/learning_sklearn/xgbregressor/xgb1.py
@@ -4,17 +4,24 @@ from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
-df = pn.read_csv("../dummy_data/problem1.csv")
+df = pn.read_csv("../dummy_data/large.csv")
 x = df[["area", "rooms", "age"]]
 y = df["rent"]
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=69)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-model = XGBRegressor(n_estimators=100, learning_rate=0.1, random_state=42)
+model = XGBRegressor(n_estimators=1000, learning_rate=0.1, random_state=42)
 
 model.fit(x_train, y_train)
-pred = model.predict(x_test)
+test_pred = model.predict(x_test)
+train_pred = model.predict(x_train)
+
 print("model: ", model)
-print("prediction: ", pred)
-print("MSE: ", mean_squared_error(y_test, pred))
-print("r2_score: ", r2_score(y_test, pred))
+print("\n")
+print("Train_prediction: ", train_pred)
+print("Train_MSE: ", mean_squared_error(y_train, train_pred))
+print("Train_r2_score: ", r2_score(y_train, train_pred))
+print("\n")
+print("Test_prediction: ", test_pred)
+print("Test_MSE: ", mean_squared_error(y_test, test_pred))
+print("Test_r2_score: ", r2_score(y_test, test_pred))
