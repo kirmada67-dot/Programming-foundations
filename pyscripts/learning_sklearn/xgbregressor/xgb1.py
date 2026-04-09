13f8a3563025f90341050ca7c1bbcf08f0530ff3
kirmada67-dot <kirmada67@gmail.com>
2026-04-09 07:26:35 +0530

    completed xgbregressor

--- pyscripts/learning_sklearn/xgbregressor/xgb1.py
+++ pyscripts/learning_sklearn/xgbregressor/xgb1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import pandas as pn
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import LeaveOneOut, cross_val_score
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
@@ -8,23 +8,27 @@ df = pn.read_csv("../dummy_data/large.csv")
 x = df[["area", "rooms", "age"]]
 y = df["rent"]
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+#x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 #new_x_train, x_val, new_y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
 
-model = XGBRegressor(n_estimators=1000, learning_rate=0.01, max_depth=3, subsample=0.7, random_state=69, colsample_bytree=0.5)
+model = XGBRegressor(n_estimators=100, learning_rate=0.1, max_depth=3, random_state=69)
+#model.fit(x_train, y_train)
+#test_pred = model.predict(x_test)
+#train_pred = model.predict(x_train)
 
 
-model.fit(x_train, y_train)
-test_pred = model.predict(x_test)
-train_pred = model.predict(x_train)
 
 print("model: ", model)
 #print("Best iteration: ", model.best_iteration)
-print("\n")
-print("Train_prediction: ", train_pred)
-print("Train_MSE: ", mean_squared_error(y_train, train_pred))
-print("Train_r2_score: ", r2_score(y_train, train_pred))
-print("\n")
-print("Test_prediction: ", test_pred)
-print("Test_MSE: ", mean_squared_error(y_test, test_pred))
-print("Test_r2_score: ", r2_score(y_test, test_pred))
+#print("\n")
+#print("Train_prediction: ", train_pred)
+#print("Train_MSE: ", mean_squared_error(y_train, train_pred))
+#print("Train_r2_s											core: ", r2_score(y_train, train_pred))
+#print("\n")
+#print("Test_prediction: ", test_pred)
+#print("Test_MSE: ", mean_squared_error(y_test, test_pred))
+#print("Test_r2_score: ", r2_score(y_test, test_pred))
+
+score = cross_val_score(model, x, y, cv=LeaveOneOut(), scoring='neg_mean_squared_error')
+print(score)
+print("Average MSE: ", -score.mean())
