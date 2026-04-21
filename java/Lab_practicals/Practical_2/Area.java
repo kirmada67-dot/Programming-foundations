d2aa070df608075d4b3482b1511ccb10a8fb0b1d
Prem Jadhav <kirmada67@gmail.com>
2026-04-21 13:47:19 +0530

    Update Area.java
    updated Lab_practical 2 code

--- java/Lab_practicals/Practical_2/Area.java
+++ java/Lab_practicals/Practical_2/Area.java
@@ -6,12 +6,22 @@ class FindArea {
 	int area(int a, int b) {
 		return a * b;
 	}
+
+	int perimeter(int a, int b) {
+		return 2 * a + 2 * b;
+	}
+
+	int perimeter(int s) {
+		return s * 4;
 }
 
 public class Area {
 	public static void main(String[] args) {
 		FindArea a = new FindArea();
 		System.out.println(a.area(10));
+		System.out.println(a.perimeter(20));
+		System.out.println(a.area(3, 7));
+		System.out.println(a.perimeter(6, 3));
 	}
 
 }
