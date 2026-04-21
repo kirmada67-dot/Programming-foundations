59fd96d4e58f293875ed87cab50e04028932c763
kirmada67-dot <kirmada67@gmail.com>
2026-04-21 13:17:16 +0530

    update

---
+++ java/Lab_practicals/Practical_3/Main.java
@@ -0,0 +1,33 @@
+class Student {
+    static String college = "ABC College";
+    int id;
+    String name;
+
+    Student(int i, String n) {
+        id = i;
+        name = n;
+    }
+
+    static void changeCollege(String c) {
+        college = c;
+    }
+
+    void display() {
+        System.out.println(id + " " + name + " " + college);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student(1, "Prem");
+        Student s2 = new Student(2, "Rahul");
+
+        s1.display();
+        s2.display();
+
+        Student.changeCollege("XYZ College");
+
+        s1.display();
+        s2.display();
+    }
+}
